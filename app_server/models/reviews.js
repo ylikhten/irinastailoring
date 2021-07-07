08d5d429f8b47ff948c31b8915b3cc03311cfa65
ylikhten <ylikhten@mymail.mines.edu>
2021-07-06 23:01:42 -0600

    set up dbRI and reviews schema

---
+++ app_server/models/reviews.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+
+var reviewSchema = new mongoose.Schema({
+    name: {type:String, required:true},
+    date: {type:Date, "default":Date.now},
+    email: {type:String, "default":""},
+    review: {type:String, required:true}
+
+});
+
+mongoose.model('Review', reviewSchema ,'reviews');
